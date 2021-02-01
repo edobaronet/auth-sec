aefb4a6076be34aed98f1679de8a2ce4d44ea73f
Edoardo <EA@MacBook-Pro-di-Edoardo.local>
2021-02-01 19:38:45 +0100

    use bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,10 +22,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-// add an ecryption key and add it as a plugin
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,17 +40,21 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        })
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    })
+
 });
 
 
@@ -67,9 +68,11 @@ app.post("/login", function(req, res){
         } else {
             console.log("pw: " + foundUser.password);
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                }) 
             }
         }
     })
